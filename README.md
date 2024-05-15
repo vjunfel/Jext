2ccd28165693418c349aa217e0766b136b57c2af
vjunfel <vjunfel@gmail.com>
2024-05-16 00:14:09 +0800

    Add next-auth and modify sidebar content

--- README.md
+++ README.md
@@ -34,3 +34,20 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+<!-- ********************************* -->
+## X.com | Twitter.com Clone Website
+
+### Dependencies
+- Next-auth
+- React-icon
+
+### DevDependencies
+- 
+
+<!-- ********************************* -->
+# console.cloud.google.com
+API and Services
+  - Credentials
+  - OAuth content on screen
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,13 @@
+'use client'
+
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react'
 import { FaXTwitter } from "react-icons/fa6";
-// import { RiTwitterXLine } from "react-icons/ri";
 import { TiHome } from "react-icons/ti";
 
 export default function Sidebar() {
+  const { data: session } = useSession()
   return (
     <div className='flex flex-col gap-4 px-3'>
       <Link href='/'>
@@ -14,9 +17,18 @@ export default function Sidebar() {
         <TiHome className='w-7 h-7'/>
         <span className='font-bold hidden xl:inline'>Home</span>
       </Link>
-      <button className='bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
+      {session ? (
+      <button className='bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'
+        onClick={() => signOut()}>
+        Sign Out
+      </button>
+      ) : (
+      <button className='bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'
+        onClick={() => signIn()}>
         Sign In
       </button>
+      )}
+      
     </div>
   )
 }
