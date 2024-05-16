68744f87b73b7730b0c37b6eb188c8b17a07e1d9
vjunfel <vjunfel@gmail.com>
2024-05-16 23:20:50 +0800

    Add mini profile to the bottom sidebar component

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -9,7 +9,19 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
-  ]
+  ],
+
+  callbacks: {
+    async session({ session, token }) {
+      session.user.username = session.user.name
+        .split(' ')
+        .join('')
+        .toLowerCase()
+      session.user.uid = token.sub
+      
+      return session
+    }
+  }
 })
 
 export { handler as GET, handler as POST }
